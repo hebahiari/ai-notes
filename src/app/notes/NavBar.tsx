2bd55fb68eb084441c90ed6a97034641160ee45e
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-03 22:21:14 -0700

    navbar created

---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="p-4 shadow">
+      <div className="flex max-w-7xl flex-wrap items-center justify-between gap-3">
+        Links
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
