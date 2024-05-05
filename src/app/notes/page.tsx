622dac049ebba15486dcd0c46bdd17e3fbb2564a
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-04 19:50:31 -0700

    fetched notes in frontend

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 import React from "react";
 
-const Notes = () => {
-  return <div>Notes</div>;
+const Notes = async () => {
+
+  const { userId } = auth()
+
+  if (!userId) throw Error("userId undefined")
+
+  const allNotes = await prisma.note.findMany({
+    where: { userId }
+  })
+
+  return (
+    <div></div>
+  )
 };
 
 export const metadata: Metadata = {
