f6699c13e58a9024b78e45361b7906e905cde158
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-07 14:13:16 -0700

    updated add note form

--- src/components/AddNoteDialog.tsx
+++ src/components/AddNoteDialog.tsx
@@ -1,23 +1,26 @@
 import { createNoteSchema, NoteSchema } from '@/lib/validation/node'
-import React from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog'
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+import LoadingButton from './ui/loading-button'
 
 interface Props {
   open: boolean,
   setOpen: (open: boolean) => void,
-
 }
 
 const AddNoteDialog = ({ open, setOpen }: Props) => {
 
   const form = useForm<NoteSchema>({
-    resolver: zodResolver(createNoteSchema)
+    resolver: zodResolver(createNoteSchema),
+    defaultValues: { title: '', content: '' }
   })
 
   async function onSubmit(input: NoteSchema) {
-    alert(input)
+    alert(JSON.stringify(input))
   }
 
   return (
@@ -26,6 +29,41 @@ const AddNoteDialog = ({ open, setOpen }: Props) => {
         <DialogHeader>
           <DialogTitle>Add Note</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Enter your note title' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='content'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder='Enter your note content' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <LoadingButton type='submit' loading={form.formState.isSubmitting}>
+                Submit
+              </LoadingButton>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
