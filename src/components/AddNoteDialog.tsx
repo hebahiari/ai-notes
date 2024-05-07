7d065417efa6783c67a92bc87d8dc3d56b533187
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-07 14:35:31 -0700

    note submittion

--- src/components/AddNoteDialog.tsx
+++ src/components/AddNoteDialog.tsx
@@ -6,6 +6,8 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import LoadingButton from './ui/loading-button'
+import toast, { Toaster } from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
 
 interface Props {
   open: boolean,
@@ -14,58 +16,76 @@ interface Props {
 
 const AddNoteDialog = ({ open, setOpen }: Props) => {
 
+  const router = useRouter()
+
   const form = useForm<NoteSchema>({
     resolver: zodResolver(createNoteSchema),
     defaultValues: { title: '', content: '' }
   })
 
   async function onSubmit(input: NoteSchema) {
-    alert(JSON.stringify(input))
+    try {
+      const response = await fetch('/api/notes', {
+        method: 'POST',
+        body: JSON.stringify(input)
+      })
+
+      if (!response.ok) throw Error("Status code: " + response.status)
+      form.reset()
+      router.refresh()
+      setOpen(false)
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong, please try again.")
+    }
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Add Note</DialogTitle>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
-            <FormField
-              control={form.control}
-              name='title'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Note Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder='Enter your note title' {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='content'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Note Content</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder='Enter your note content' {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <LoadingButton type='submit' loading={form.formState.isSubmitting}>
-                Submit
-              </LoadingButton>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Toaster />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add Note</DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+              <FormField
+                control={form.control}
+                name='title'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Note Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Enter your note title' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='content'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Note Content</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder='Enter your note content' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <LoadingButton type='submit' loading={form.formState.isSubmitting}>
+                  Submit
+                </LoadingButton>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
 
