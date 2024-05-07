90842a7a3c9a5c0f90e092a8a6c215143689f729
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-07 13:36:41 -0700

    added dialog to navbar

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -1,32 +1,42 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
 import logo from "@/assets/logo.png"
 import { UserButton } from "@clerk/nextjs"
 import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
+import { useState } from "react"
+import AddNoteDialog from "@/components/AddNoteDialog"
 
 const NavBar = () => {
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false)
+
   return (
-    <div className="p-4 shadow">
-      <div className="flex max-w-7xl flex-wrap items-center justify-between gap-3">
-        <Link href="/notes" className="flex items-center gap-3">
-          <Image src={logo} alt="SmartNotes" width={20} height={20} />
-          <span className="font-bold">SmartNotes</span>
-        </Link>
-        <div className='flex items-center gap-3'>
-          <Button>
-            <Plus size={20} className="mr-2" />
-            Add Note
-          </Button>
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: { avatarBox: { width: '2.5rem', height: '2.5rem' } }
-            }}
-          />
+    <>
+      <div className="p-4 shadow">
+        <div className="flex max-w-7xl flex-wrap items-center justify-between gap-3">
+          <Link href="/notes" className="flex items-center gap-3">
+            <Image src={logo} alt="SmartNotes" width={20} height={20} />
+            <span className="font-bold">SmartNotes</span>
+          </Link>
+          <div className='flex items-center gap-3'>
+            <Button onClick={() => setShowAddNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                elements: { avatarBox: { width: '2.5rem', height: '2.5rem' } }
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />
+    </>
   )
 }
 
