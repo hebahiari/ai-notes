5939850327958ca81253ac3c4ac0ab95090db279
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-08 18:38:42 -0700

    AIChatBox basic styling

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -1,5 +1,8 @@
 import { cn } from '@/lib/utils';
 import { useChat } from 'ai/react'
+import { XCircle } from 'lucide-react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 interface Props {
     open: boolean;
@@ -18,8 +21,27 @@ const AIChatBox = ({ open, onClose }: Props) => {
     } = useChat()
 
     return (
-        <div className={cn('bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36', open ? 'fixed' : 'hidden')}>
-            AIChatBox
+        <div
+            className={cn('bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36',
+                open ? 'fixed' : 'hidden'
+            )}
+        >
+            <button onClick={onClose} className='mb-1 ms-auto block'>
+                <XCircle size={30} />
+            </button>
+            <div className='flex h-[600px] flex-col rounded bg-background border shadow-lg'>
+                <div className='h-full'>Messages</div>
+                <form onSubmit={handleSubmit} className='m-3 flex gap-2'>
+                    <Input
+                        value={input}
+                        onChange={handleInputChange}
+                        placeholder='Say something...'
+                    />
+                    <Button type='submit'>
+                        Send
+                    </Button>
+                </form>
+            </div>
         </div>
     )
 }
