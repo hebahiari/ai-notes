bb853d6e12c063565103f966ea7bdb3cb06f4f1c
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-05-10 20:12:53 -0700

    chat messages styling

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -1,9 +1,11 @@
 import { cn } from '@/lib/utils';
 import { useChat } from 'ai/react'
-import { XCircle } from 'lucide-react';
+import { Bot, XCircle } from 'lucide-react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Message } from 'ai';
+import { useUser } from '@clerk/nextjs';
+import Image from 'next/image';
 
 interface Props {
     open: boolean;
@@ -27,11 +29,11 @@ const AIChatBox = ({ open, onClose }: Props) => {
                 open ? 'fixed' : 'hidden'
             )}
         >
-            <button onClick={onClose} className='mb-1 ms-auto block'>
-                <XCircle size={30} />
+            <button onClick={onClose} className='mb-1 ms-auto block absolute top-3 right-3'>
+                <XCircle size={25} />
             </button>
             <div className='flex h-[600px] flex-col rounded bg-background border shadow-lg'>
-                <div className='h-full'>
+                <div className='h-full mt-9 px-3 py-3 overflow-y-auto'>
                     {messages.map((message) => (
                         <ChatMessage message={message} key={message.id} />
                     ))}
@@ -54,10 +56,33 @@ const AIChatBox = ({ open, onClose }: Props) => {
 export default AIChatBox
 
 function ChatMessage({ message: { role, content } }: { message: Message }) {
+
+    const { user } = useUser()
+
+    const isAIMessage = role === 'assistant'
+
     return (
-        <div>
-            <div>{role}</div>
-            <div>{content}</div>
+        <div className={cn(
+            "mb-3 flex items-center",
+            isAIMessage ? 'justify-end ms-5' : 'justify-start me-5'
+        )}>
+            {!isAIMessage && user?.imageUrl && (
+                <Image
+                    src={user.imageUrl}
+                    alt='User Image'
+                    width={40}
+                    height={40}
+                    className='mr-2 rounded-full'
+                />
+            )}
+            <p className={cn(
+                'whitespace-pre-line rounded-lg border px-3 py-2',
+                isAIMessage ? 'bg-background' : 'bg-primary text-primary-foreground'
+            )}
+            >
+                {content}
+            </p>
+            {isAIMessage && <Bot className='ml-2 shrink-0' size={30} />}
         </div>
     )
 }
